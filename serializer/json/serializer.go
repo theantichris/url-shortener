9705463c4ea512ba3215713215469e2b9ef106c6
Christopher Lamm <chris@theantichris.com>
2020-09-16 07:46:07 -0400

    added JSON serializer

---
+++ serializer/json/serializer.go
@@ -0,0 +1,32 @@
+package json
+
+import (
+	"encoding/json"
+
+	"github.com/pkg/errors"
+	"github.com/theantichris/url-shortener/shortener"
+)
+
+// Redirect holds a JSON implementation of shortener.Redirect.
+type Redirect struct{}
+
+// Decode decodes a Redirect from JSON
+func (r *Redirect) Decode(input []byte) (*shortener.Redirect, error) {
+	redirect := &shortener.Redirect{}
+
+	if err := json.Unmarshal(input, redirect); err != nil {
+		return nil, errors.Wrap(err, "serializer.Redirect.Decode")
+	}
+
+	return redirect, nil
+}
+
+// Encode encodes a Redirect to JSON.
+func (r *Redirect) Encode(input *shortener.Redirect) ([]byte, error) {
+	rawMsg, err := json.Marshal(input)
+	if err != nil {
+		return nil, errors.Wrap(err, "serializer.Redirect.Encode")
+	}
+
+	return rawMsg, nil
+}
