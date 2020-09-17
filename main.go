6c943422fd17146b7dd118daf1f496a00d64843d
Christopher Lamm <chris@theantichris.com>
2020-09-17 10:27:39 -0400

    added executable function

--- main.go
+++ main.go
@@ -1,5 +1,83 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/theantichris/url-shortener/api"
+	"github.com/theantichris/url-shortener/repository/mongodb"
+	"github.com/theantichris/url-shortener/repository/redis"
+	"github.com/theantichris/url-shortener/shortener"
+)
+
+func main() {
+	repo := chooseRepo()
+	service := shortener.NewRedirectService(repo)
+	handler := api.NewHandler(service)
+
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/{code}", handler.Get)
+	r.Post("/", handler.Post)
+
+	errs := make(chan error, 2)
+
+	go func() {
+		fmt.Printf("Listening on port %s\n", httpPort())
+		errs <- http.ListenAndServe(httpPort(), r)
+	}()
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT)
+		errs <- fmt.Errorf("%s", <-c)
+	}()
+
+	fmt.Printf("Terminalted %s", <-errs)
+}
+
+func httpPort() string {
+	port := "8000"
+	if os.Getenv("PORT") != "" {
+		port = os.Getenv("PORT")
+	}
+
+	return fmt.Sprintf(":%s", port)
+}
+
+func chooseRepo() shortener.RedirectRepository {
+	switch os.Getenv("DB_TYPE") {
+	case "redis":
+		redisURL := os.Getenv("REDIS_URL")
+		repo, err := redis.NewRedisRepository(redisURL)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		return repo
+	case "mongo":
+		mongoURL := os.Getenv("MONGO_URL")
+		mongoDB := os.Getenv("MONGO_DB")
+		mongoTimeout, _ := strconv.Atoi(os.Getenv("MONGO_TIMEOUT"))
+
+		repo, err := mongodb.NewMongoRepository(mongoURL, mongoDB, mongoTimeout)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		return repo
+	}
+
+	return nil
 }
